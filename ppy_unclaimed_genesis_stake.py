6e76847b685d418cc305cefb9b1ffe11305763ed
Rob Konsdorf <robertkonsdorf@gmail.com>
2018-01-12 13:42:05 -0500

    Add newlines

--- ppy_unclaimed_genesis_stake.py
+++ ppy_unclaimed_genesis_stake.py
@@ -6,12 +6,12 @@ genesis_data = json.load(open('genesis.json'))
 
 crowdsale_and_vesting_balances = genesis_data["initial_balances"] + genesis_data["initial_vesting_balances"]
 
-print("Total number of crowdsale and vesting balances in genesis: %s" % len(crowdsale_and_vesting_balances))
+print("Total number of crowdsale and vesting balances in genesis:\n %s" % len(crowdsale_and_vesting_balances))
 
 claims_data = json.load(open('claims.json'))
 
-print("Total number of balance claims processed as of January 10, 2018: %s" % len(claims_data))
-print("Total unclaimed crowdsale and vesting genesis balances remaining: %s" % (len(crowdsale_and_vesting_balances) - len(claims_data)))
+print("Total number of balance claims processed as of January 10, 2018:\n %s" % len(claims_data))
+print("Total unclaimed crowdsale and vesting genesis balances remaining:\n %s" % (len(crowdsale_and_vesting_balances) - len(claims_data)))
 
 unclaimed_crowdsale_genesis_entries = crowdsale_and_vesting_balances.copy()
 total_stake_claimed = 0
@@ -41,10 +41,10 @@ for genesis_entry in unclaimed_crowdsale_genesis_entries:
 normalized_total_stake_claimed = total_stake_claimed / 100000
 normalized_total_stake_unclaimed = total_unclaimed_stake / 100000
 
-print("Total claimed crowdsale and vesting stake: %s PPY" % normalized_total_stake_claimed)
-print("Total claimed crowdsale and vesting stake as percentage of total supply: %s" % ((normalized_total_stake_claimed / TOTAL_PPY_SUPPLY) * 100) + " %")
-print("Total unclaimed crowdsale and vesting stake: %s PPY" % normalized_total_stake_unclaimed)
-print("Total unclaimed crowdsale stake as percentage of total supply: %s" % ((normalized_total_stake_unclaimed / TOTAL_PPY_SUPPLY) * 100) + " %")
+print("Total claimed crowdsale and vesting stake:\n %s PPY" % normalized_total_stake_claimed)
+print("Total claimed crowdsale and vesting stake as percentage of \ntotal supply:\n %s" % ((normalized_total_stake_claimed / TOTAL_PPY_SUPPLY) * 100) + " %")
+print("Total unclaimed crowdsale and vesting stake:\n %s PPY" % normalized_total_stake_unclaimed)
+print("Total unclaimed crowdsale stake as percentage of total supply:\n %s" % ((normalized_total_stake_unclaimed / TOTAL_PPY_SUPPLY) * 100) + " %")
 print("Known unclaimed genesis owner / balances")
 print("========================================")
 print(remaining_unclaimed_genesis_entries)
